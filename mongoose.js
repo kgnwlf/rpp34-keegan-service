25abb10803289a565eef0094a2a284b9d7505d7d
Keegan Wolf <kwolf313@live.com>
2022-05-20 10:24:53 -0500

    Mongo Schema

---
+++ mongoose.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+  question_id: Number,
+  question_body: String,
+  question_date: String,
+  asker_name: String,
+  question_helpfulness: Number,
+  reported: Boolean,
+  answers: [{ type: mongoose.Types.ObjectId, ref: 'answers' }]
+});
+
+const answerSchema = new mongoose.Schema({
+  question_id: { type: mongoose.Types.ObjectId, ref: 'questions' } ,
+  answer_id: Number,
+  body: String,
+  date: String,
+  answerer_name: String,
+  helpfulness: Number,
+  reported: Boolean,
+  photos: [{ type: mongoose.Types.ObjectId, ref: 'photos' }]
+});
+
+const photoSchema = new mongoose.Schema({
+  answer_id: { type: mongoose.Types.ObjectId, ref: 'answers' } ,
+  photo_url: String
+});
+
+const question = mongoose.model('questions', questionSchema);
+const answer = mongoose.model('answers', answerSchema);
+const photo = mongoose.model('photos', photoSchema);
+
+module.exports = {
+  question: question,
+  answer: answer,
+  photo: photo
+};
