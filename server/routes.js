c6ed69d48b9eee6fd5dd003937bbf46c011a9d0a
Keegan Wolf <kwolf313@live.com>
2022-06-29 15:29:03 -0500

    Adding console logs for instances

--- server/routes.js
+++ server/routes.js
@@ -127,11 +127,14 @@ app.put('/qa/answers/:answer_id/report', async (req, res) => {
   // expects an answer id (in url)
   // 204
 
+  console.log(req.params.answer_id);
+
   db.report('answers', parseInt(req.params.answer_id))
   .then(() => {
     res.status(204).end();
   })
-  .catch(() => {
+  .catch((err) => {
+    console.log(err);
     res.status(500).end();
   })
 
